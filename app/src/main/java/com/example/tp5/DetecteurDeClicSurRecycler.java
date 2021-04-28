542c58a6243d14e7acd8fc51346a1e13d16a8f5c
aghilas5 <aithadjiaghilas7@gmail.com>
2021-04-28 10:45:00 +0200

    Un de mes commits

---
+++ app/src/main/java/com/example/tp5/DetecteurDeClicSurRecycler.java
@@ -0,0 +1,9 @@
+package com.example.tp5;
+
+
+import android.view.View;
+
+public interface DetecteurDeClicSurRecycler {
+    public void clicSurRecyclerItem(int position, View v);
+}
+
---
+++ app/src/main/java/com/example/tp5/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.tp5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements DetecteurDeClicSurRecycler{
+    private RecyclerView mRecyclerView;
+    private MonRecycleViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    CoordinatorLayout mcoordinatorLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MonRecycleViewAdapter(getDataSource());
+        mRecyclerView.setAdapter(mAdapter);
+        mcoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+     //   RecyclerView.ItemDecoration itemDecoration =
+             //   new DividerItemDecoration(this, R.drawable.divider);
+      //  mRecyclerView.addItemDecoration(itemDecoration);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter.setDetecteurDeClicSurRecycler(this);
+    }
+
+    private ArrayList<Donnee> getDataSource() {
+        ArrayList results = new ArrayList<Donnee>();
+        for (int index = 0; index < 20; index++) {
+            Donnee obj = new Donnee("Texte principal " + index,"Texte auxiliaire " + index);
+            results.add(index, obj);
+        }
+        return results;
+    }
+
+
+    @Override
+    public void clicSurRecyclerItem(int position, View v) {
+        Snackbar.make(mcoordinatorLayout, " Clic sur l'item " + position, Snackbar.LENGTH_LONG).show();
+
+
+
+    }
+}
